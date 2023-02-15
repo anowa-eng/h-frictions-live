0d64aa478719fbf09c5e24395f19cf64bad1a297
anowa-eng <georgemorrissette2010@gmail.com>
2023-02-15 05:56:39 +0000

    API endpoint class

--- api/internal/endpoint.py
+++ api/internal/endpoint.py
@@ -1,21 +1,38 @@
 from flask import Blueprint, request
 import csvfile
+import json
 
 class Endpoint:
     def __init__(self, file_name):
         self.file = f'db/{file_name}.csv'
         self.blueprint = Blueprint(file_name, file_name)
+    def load_data(self):
+        return csvfile.load(self.file)
+    def rows(self):
+        return len(self.load_data())
     # @self.blueprint.route(f'/api/{file_name}')
     def create(self, **kwargs):
-        data = csvfile.load(self.file)
-        data.append(kwargs)
+        data = self.load_data()
+        data.append({
+            **kwargs,
+            'ID': self.rows()
+        })
         data.sync()
-    def __compare(list, **kwargs):
-        new_list = list
-            
-
-    def get_all(self, **kwargs):
-        data = csvfile.load(self.file)
-        return data
-
-            
+    def retrieve(self, ids):
+        data = self.load_data()
+        print(f'\n\ndata: {json.dumps(data)}\n\n')
+        filtered_data = list(filter(
+            lambda item: int(item.ID) in ids,
+            data
+        ))
+        return filtered_data[0] if len(ids) == 1 else filtered_data
+    def update(self, ids, new_data):
+        data = self.load_data()
+        for id in ids:
+            data[id] = {**new_data, 'id': id}
+        data.sync()
+    def delete(self, ids):
+        data = self.load_data()
+        for id in ids:
+            data[id] = new_data
+        data.sync()
